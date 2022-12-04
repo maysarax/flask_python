d9ed1d4dca0374945516a0d986ffee70958a3e21
maysara <maysarax@users.noreply.github.com>
2022-12-04 22:28:12 +0200

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -1,3 +1,19 @@
 sudo yum install python3-djangorestframework
 django-admin --version
+sudo yum install python3-django
+
+
+mkdir ~/newproject
+cd ~/newproject
+virtualenv newenv
+source newenv/bin/activate
+pip install django
+django-admin --version
+
+
+django-admin startproject one
+cd one
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver 0.0.0.0:8000
 
